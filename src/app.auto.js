625a9fb6fe4cc145d8da46e6be521bb7650b8d9a
liuridong <1038151951@qq.com>
2022-06-05 17:08:26 +0800

    init

---
+++ src/app.auto.js
@@ -0,0 +1,95 @@
+/**
+ * ovine 应用配置。文档：https://ovine.igroupes.com/org/docs/advance/configurations#%E5%BA%94%E7%94%A8%E9%85%8D%E7%BD%AE
+ * 路径别名映射:
+ * '@core/*': '@core/*'
+ * '～/*': '/src/*'
+ */
+
+const config = {
+  env: {
+    default: {
+      disableLimit: true,
+      domains: {
+        api: 'http://127.0.0.1:9011',
+      },
+    },
+    // 本地开发
+    localhost: {
+      domains: {
+        api: 'http://127.0.0.1:9011',
+      },
+    },
+    // 测试环境
+    staging: {
+      domains: {
+        api: 'http://192.168.31.106',
+      },
+    },
+    // 生产环境
+    production: {
+      domains: {
+        api: 'http://192.168.31.106',
+      },
+    },
+  },
+  entry: [
+    {
+      type: 'preset-route', // 路由组件
+      path: '/login',
+      pathToComponent: true,
+    },
+    {
+      type: 'private-route', // 私有路由
+      path: '/',
+      redirect: '/login',
+      onAuth: () => {
+        return true
+      },
+      children: {
+        type: 'aside-layout',
+        header: {
+          showDevItem: false,
+          brand: {
+            // 公司品牌
+            logo: '/static/images/archlinux-logo.png',
+            title: 'HomeKit',
+            link: {
+              title: 'dashboard',
+              href: '/',
+            },
+          },
+          items: [
+            {
+              type: 'item-setting',
+              align: 'right',
+            },
+          ],
+        },
+        routes: [
+          // 应用内路由
+          {
+            nodePath: '/',
+            label: '菜单目录',
+            children: [
+              {
+                path: '/',
+                label: 'Dashboard',
+                nodePath: 'dashboard',
+                exact: true,
+                pathToComponent: 'dashboard',
+                sideVisible: false,
+              },
+              {
+                label: '设备列表',
+                icon: 'fa fa-align-justify',
+                nodePath: 'iots',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+}
+
+export default config
---
+++ src/pages/iots/index.js
@@ -0,0 +1,65 @@
+export const schema = {
+  type: "page",
+  body: {
+    type: "crud",
+    api: "GET:/api/hk/iots/allIots?pageIndex=${page}&pageSize=${perPage}",
+    columns: [
+      {
+        name: "iotCode",
+        label: "设备编码",
+        fixed: "left",
+      },
+      {
+        name: "iotName",
+        label: "设备名称",
+        fixed: "left",
+      },
+      {
+        label: "设备类型",
+        type: "mapping",
+        name: "deviceType",
+        map: {
+          LIGHT: "灯",
+          UNKNOWN: "未知设备",
+          "*": "其他：${deviceType}",
+        },
+      },
+      {
+        label: "开关",
+        type: "mapping",
+        name: "switchValue",
+        map: {
+          ON: "<span class='label label-success'>开</span>",
+          OFF: "<span class='label label-warning'>关</span>",
+          "*": "其他：${switchValue}",
+        },
+      },
+      {
+        name: "ipAddr",
+        label: "IP地址",
+      },
+      {
+        name: "onlineState",
+        label: "在线状态",
+        type: "mapping",
+        map: {
+          OFFLINE: "<span class='label label-danger'>离线</span>",
+          ONLINE: "<span class='label label-success'>在线</span>",
+          "*": "其他：${onlineState}",
+        },
+      },
+      {
+        name: "lastBeatTime",
+        type: "date",
+        format: "YYYY-MM-DD HH:mm:ss",
+        label: "上一次心跳时间",
+      },
+      {
+        name: "cts",
+        type: "date",
+        format: "YYYY-MM-DD HH:mm:ss",
+        label: "注册时间",
+      },
+    ],
+  },
+};
